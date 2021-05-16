f304d0c742a0c59f8eb5ed93346d34f764a751fa
Foba4 <sidyfoba@gmail.com>
2021-05-16 22:41:48 +0000

    first commit

---
+++ Migrations/20210516223151_RowVersion.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class RowVersion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourseInstructor_Instructors_InstructorsID",
+                table: "CourseInstructor");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Departments_Instructors_InstructorID",
+                table: "Departments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollment_Course_CourseID",
+                table: "Enrollment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollment_Student_StudentID",
+                table: "Enrollment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OfficeAssignments_Instructors_InstructorID",
+                table: "OfficeAssignments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Instructors",
+                table: "Instructors");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Enrollment",
+                table: "Enrollment");
+
+            migrationBuilder.RenameTable(
+                name: "Instructors",
+                newName: "Instructor");
+
+            migrationBuilder.RenameTable(
+                name: "Enrollment",
+                newName: "Enrollments");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Enrollment_StudentID",
+                table: "Enrollments",
+                newName: "IX_Enrollments_StudentID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Enrollment_CourseID",
+                table: "Enrollments",
+                newName: "IX_Enrollments_CourseID");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ConcurrencyToken",
+                table: "Departments",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Instructor",
+                table: "Instructor",
+                column: "ID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Enrollments",
+                table: "Enrollments",
+                column: "EnrollmentID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseInstructor_Instructor_InstructorsID",
+                table: "CourseInstructor",
+                column: "InstructorsID",
+                principalTable: "Instructor",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Departments_Instructor_InstructorID",
+                table: "Departments",
+                column: "InstructorID",
+                principalTable: "Instructor",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollments_Course_CourseID",
+                table: "Enrollments",
+                column: "CourseID",
+                principalTable: "Course",
+                principalColumn: "CourseID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollments_Student_StudentID",
+                table: "Enrollments",
+                column: "StudentID",
+                principalTable: "Student",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OfficeAssignments_Instructor_InstructorID",
+                table: "OfficeAssignments",
+                column: "InstructorID",
+                principalTable: "Instructor",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourseInstructor_Instructor_InstructorsID",
+                table: "CourseInstructor");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Departments_Instructor_InstructorID",
+                table: "Departments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollments_Course_CourseID",
+                table: "Enrollments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollments_Student_StudentID",
+                table: "Enrollments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OfficeAssignments_Instructor_InstructorID",
+                table: "OfficeAssignments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Instructor",
+                table: "Instructor");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Enrollments",
+                table: "Enrollments");
+
+            migrationBuilder.DropColumn(
+                name: "ConcurrencyToken",
+                table: "Departments");
+
+            migrationBuilder.RenameTable(
+                name: "Instructor",
+                newName: "Instructors");
+
+            migrationBuilder.RenameTable(
+                name: "Enrollments",
+                newName: "Enrollment");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Enrollments_StudentID",
+                table: "Enrollment",
+                newName: "IX_Enrollment_StudentID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Enrollments_CourseID",
+                table: "Enrollment",
+                newName: "IX_Enrollment_CourseID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Instructors",
+                table: "Instructors",
+                column: "ID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Enrollment",
+                table: "Enrollment",
+                column: "EnrollmentID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseInstructor_Instructors_InstructorsID",
+                table: "CourseInstructor",
+                column: "InstructorsID",
+                principalTable: "Instructors",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Departments_Instructors_InstructorID",
+                table: "Departments",
+                column: "InstructorID",
+                principalTable: "Instructors",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollment_Course_CourseID",
+                table: "Enrollment",
+                column: "CourseID",
+                principalTable: "Course",
+                principalColumn: "CourseID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollment_Student_StudentID",
+                table: "Enrollment",
+                column: "StudentID",
+                principalTable: "Student",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OfficeAssignments_Instructors_InstructorID",
+                table: "OfficeAssignments",
+                column: "InstructorID",
+                principalTable: "Instructors",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
